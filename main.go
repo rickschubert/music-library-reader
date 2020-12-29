5fd83bf1f4389a13b5ad80034596e9a4329b81f6
Rick Schubert <rickschubert@gmx.de>
2020-12-29 17:41:44 +0000

    add sorting functionality

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/dhowden/tag"
+	"github.com/Songmu/prompter"
+	"github.com/bradfitz/slice"
 	"os"
 	"path/filepath"
 	"regexp"
+	"log"
 )
 
 func logDescription() {
@@ -100,13 +103,39 @@ func getAllFileNamesInDirectoryRecursively(directoryPath string) []string {
 	return files
 }
 
+func promptForDirectory() string {
+	directory := prompter.Prompt("Please provide us with the path of the directory where all your music lies. I.e. C:\\Music", "")
+	if directory == "" {
+		log.Fatal("You need to enter a valid path")
+	}
+	return directory
+}
+
+// If byTitle is false, sorts songs by artist name
+func sortSongs(songs []Song, byTitle bool) {
+	slice.Sort(songs[:], func(i, j int) bool {
+		if byTitle {
+			return songs[i].Title < songs[j].Title
+		} else {
+			return songs[i].Artist < songs[j].Artist
+		}
+	})
+}
+
 func main() {
 	logDescription()
 
 	mainDirectory := "G:\\Musik\\0 - Restmusik"
+	// TODO: Comment back in if all is done
+	// mainDirectory := promptForDirectory()
+
+	sortSongsByTitle := true
+	// TODO: Comment back in if all is done
+	// var sortSongsByTitle bool = prompter.YN("Should we sort the list by title? If you say no, we will sort by artist.", true)
 
 	files := getAllFileNamesInDirectoryRecursively(mainDirectory)
 	songs := collectSongsFromFileNames(files)
+	sortSongs(songs, sortSongsByTitle)
 
 	fmt.Println(songs)
 }
